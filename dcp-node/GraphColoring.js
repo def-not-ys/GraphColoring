ee03d3aa935285347e5c82d6cc62b0a06c2e140a
def-not-ys <luys1201@gmail.com>
2020-08-16 09:59:53 -0700

    refactrored Graph

--- dcp-node/GraphColoring.js
+++ dcp-node/GraphColoring.js
@@ -25,8 +25,10 @@ class Vertex {
 
 class Graph {
 
-    constructor() {
+    constructor(n, maxDegree) {
         this._adjList = [];
+        this._maxDegree = maxDegree;
+        this.generateGraph(n, maxDegree)
     }
 
     get adjList() {
@@ -120,24 +122,86 @@ class Graph {
         }
     }
 
+    // generate a graph with n random distinct value[0, n-1], default color vertices, returns the graph
+    generateGraph(n, maxDegree) {
+        var m = 0;
+        // generate n vertices and add them to graph 
+        for (var i = 0; i < n; i++) {
+            vertex = new Vertex(i);
+            vertex.color = -1;
+            this.addVertex(vertex);       
+        }
+        // generate edges between vertices and add them to graph 
+        for (var entry in this.adjList) {
+            var d = randomInteger(1, maxDegree); // connected graph 
+
+            // console.log("degree:", d);
+
+            var vertex = this.adjList[entry].key;
+            for (var i = 1; i <= d; i++) {
+                var x = randomInteger(0, n-1);
+                var neighbourKey = this.findVertexByVal(x); // choose a random vertex
+                var neighbour = this.adjList[neighbourKey].key;
+
+                if (this.findEdgeByVertex(neighbour).length < maxDegree) {
+                    this.addEdge(vertex, neighbour);
+                    m++;
+                }            
+
+                if (this.findEdgeByVertex(vertex).length >= maxDegree) {
+                    break;
+                }
+            }
+        }
+
+        console.log("generated graph with", n, "vertices and", m , "edges");
+    }
+
+    // solve coloring problem in graph, returns unsolvable if unsolvable 
+    solve() { 
+        let queue = new Queue();
+        var start = this.adjList[0].key;
+        queue.enqueue(start);
+
+        while (!queue.isEmpty()) {
+            var vertex = queue.dequeue().data;
+            var edges = this.findEdgeByVertex(vertex);
+            var colors = generateColors(this._maxDegree);
+
+            // console.log("reset colors: ", colors);
+
+            for (var e in edges) {
+                if (edges[e].color >= 0) {
+                    colors.delete(edges[e].color);
+                    // console.log("delete neighbouts'color: ", edges[e].color);
+                } else {
+                    queue.enqueue(edges[e]);
+                }
+            }
+
+            // console.log("after checking neighbouts'colors: ", colors);
+
+            console.assert(colors.size > 0);
+
+            // const it = colors.values();
+            // console.log(it.next.value);
+
+            // console.log(Array.from(colors));
+
+            vertex.color = Array.from(colors)[0];
+                    
+        }
+
+        console.log("graph solved!")
+
+        this.printGraph();
+
+    }
+
 }
-
-// const colors = {
-//     RED: 'red',
-//     YELLOW: 'yellow',
-//     BLUE: 'blue',
-//     GREEN: 'green'
-// }
-
 // generate random integer in [min, max]
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// get random item from a Set
-function getRandomItem(set) {
-    let items = Array.from(set);
-    return items[Math.floor(Math.random() * items.length)];
 }
 
 // generates a set of colors with d distinct colors
@@ -149,83 +213,7 @@ function generateColors(d) {
     return set;
 }
 
-// generate a graph with n random distinct value[0, n-1], default color vertices, returns the graph
-function generateGraph(n, maxDegree) {
-    let graph = new Graph();
-    
-    // generate n vertices and add them to graph 
-    for (var i = 0; i < n; i++) {
-        vertex = new Vertex(i);
-        vertex.color = -1;
-        graph.addVertex(vertex);       
-    }
-    // generate edges between vertices and add them to graph 
-    for (var entry in graph.adjList) {
-        var d = randomInteger(1, maxDegree); // connected graph 
 
-        // console.log("degree:", d);
+let graph = new Graph(500,400);
+graph.solve();
 
-        var vertex = graph.adjList[entry].key;
-        for (var i = 1; i <= d; i++) {
-            var x = randomInteger(0, n-1);
-            var neighbourKey = graph.findVertexByVal(x); // choose a random vertex
-            var neighbour = graph.adjList[neighbourKey].key;
-
-            if (graph.findEdgeByVertex(neighbour).length < maxDegree) {
-                graph.addEdge(vertex, neighbour);
-            }            
-
-            if (graph.findEdgeByVertex(vertex).length >= maxDegree) {
-                break;
-            }
-        }
-    }
-    return graph;
-}
-
-// solve coloring problem in graph, returns unsolvable if unsolvable 
-function solve(n, maxDegree) { 
-    // let allColors = generateColors(maxDegree);
-    let graph = generateGraph(n, maxDegree);
-
-    // console.log(colors);
-    // graph.printGraph();    
-
-    let queue = new Queue();
-    var start = graph.adjList[0].key;
-    queue.enqueue(start);
-
-    while (!queue.isEmpty()) {
-        var vertex = queue.dequeue().data;
-        var edges = graph.findEdgeByVertex(vertex);
-        var colors = generateColors(maxDegree);
-
-        // console.log("reset colors: ", colors);
-
-        for (var e in edges) {
-            if (edges[e].color >= 0) {
-                colors.delete(edges[e].color);
-                // console.log("delete neighbouts'color: ", edges[e].color);
-            } else {
-                queue.enqueue(edges[e]);
-            }
-        }
-
-        // console.log("after checking neighbouts'colors: ", colors);
-
-        console.assert(colors.size > 0);
-
-        // const it = colors.values();
-        // console.log(it.next.value);
-
-        console.log(Array.from(colors));
-
-        vertex.color = Array.from(colors)[0];
-                
-    }
-
-    graph.printGraph();
-
-}
-
-solve(10, 5);
